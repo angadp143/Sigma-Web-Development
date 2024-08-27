c23a99a465eb93c6665af29708146dc57d617226
Angad Prajapati <angad2526@gmail.com>
2024-08-27 22:32:02 +0530

    Day54

---
+++ Lecture_54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Hey I am full stack web developer...")
+console.log("Dream Company is Google, Microsoft, etc...")
+console.log("I am ethusiastic and holistic person...")
---
+++ Lecture_54/script.js
@@ -0,0 +1,21 @@
+alert("Hello World")
+
+console.log("Hey I am full stack web developer...")
+console.log("Dream Company is Google, Microsoft, etc...")
+console.log("I am ethusiastic and holistic person...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and burn your pc")
+if (isTrue) {
+    console.log("pc is burning")
+}
+else {
+    console.log("pc is not burning")
+}
+
+
+console.log("your number is" + a)
+
+document.title = "Hey I am good"
+
+document.body.style.backgroundColor = "red"
