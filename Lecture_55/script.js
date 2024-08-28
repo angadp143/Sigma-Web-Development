f834e8b530c20e136ea6fa697ebb7843509fedc9
Angad Prajapati <angad2526@gmail.com>
2024-08-28 23:28:29 +0530

    Day55

---
+++ Lecture_55/script.js
@@ -0,0 +1,32 @@
+var a = 5;
+let b = 6;
+let c = "Angad";
+let _a = "Prince";
+//var 55a = "Reeta";  //not allowed
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c);    //identify the type
+// const a1=6;
+// a1 = a1+1;  //not allowed
+{
+    var a = 66;
+    console.log(a)
+}
+console.log(a)
+let x = "Angad bhai";
+let y = 22;
+let z = 21.51;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+let o = {
+    "name": "Angad",
+    "job code": 5100,
+    "is_handsome": true
+}
+console.log(o)
+o.salary = "100crores"
+console.log(o)
+o.salary = "5000crores"
+console.log(o)
