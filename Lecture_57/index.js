091feb0e1fa03fa612829dd02d7ad596250924a5
Angad Prajapati <angad2526@gmail.com>
2024-08-31 01:53:24 +0530

    Day57

---
+++ Lecture_57/index.js
@@ -0,0 +1,48 @@
+// five types of loops in javascript:
+// 1) for loop
+// 2) for in loop
+// 3) for of loop 
+// 4) while loop
+// 5)do-while loop
+
+
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+// console.log(a)
+// console.log(a + 1)
+// console.log(a + 2)
+// console.log(a + 3)
+// console.log(a + 4)
+
+// for (let i = 0; i < 101; i++) {
+//     console.log(a + i);
+
+// }
+
+// let obj = {
+//     name: "Angad",
+//     role: "Coder",
+//     company: "Amazon"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for (const c of "Angad") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i)
+//     i++
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
