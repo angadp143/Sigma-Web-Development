378d2ca0b49671ca242c2a47be6550fc1d977f63
Angad Prajapati <angad2526@gmail.com>
2024-09-02 00:02:18 +0530

    Day60

---
+++ Lecture_60/String.js
@@ -0,0 +1,25 @@
+console.log("This is strings tutorial")
+let a = "Angad";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+//console.log(a[5])
+
+console.log(a.length)
+
+let real_name = "Prince"
+let friend = "Rampal"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "Shivu"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0, 5))
+console.log(b.slice(0))
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "Nayantara"))
