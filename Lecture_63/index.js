3c7bbd76d6196051599fa83af8a26ad6e921fe3e
Angad Prajapati <angad2526@gmail.com>
2024-09-03 22:15:07 +0530

    Day63

---
+++ Lecture_63/index.js
@@ -0,0 +1,28 @@
+// let arr=[1, 2, 4, 5, 7]
+// //   Index 0  1  2  3  4
+
+// arr[0]=566;
+// // console.log(arr);
+// // console.log(arr.lenght)
+// // console.log(arr[4])
+
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+// console.log(arr.pop())
+// console.log(arr.push(501))
+
+
+let a1 = [1, 2, 3];
+let a2 = [4, 5, 6];
+let a3 = [7, 8, 9];
+
+let result = a1.concat(a2, a3);
+console.log(result);
+
+
+// let numbers=[1, 2, 3, 4, 5]
+// numbers.splice(1, 2)
+// let numbers=[1, 2, 3, 4, 5]
+// numbers.splice(1, 3)
+// let numbers=[1, 2, 3, 4, 5]
+// numbers.splice(1, 3, 222, 333)
